d8499826955c020d897b10584179ca517eb1bb96
Muqsit <hackhack05@gmail.com>
2018-07-20 17:43:07 +0000

    Remove unused imports

--- src/muqsit/bedrockfix/EventListener.php
+++ src/muqsit/bedrockfix/EventListener.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace muqsit\bedrockfix;
 
-use pocketmine\entity\Entity;
 use pocketmine\event\Listener;
 use pocketmine\event\server\DataPacketReceiveEvent;
 use pocketmine\network\mcpe\protocol\SetEntityDataPacket;
