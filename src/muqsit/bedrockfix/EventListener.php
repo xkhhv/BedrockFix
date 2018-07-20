1a5a06c8a7ee8bf4fe0702550c1cdfae686ce920
KHAV <khweld454@gmail.com>
2018-07-20 22:36:09 +0300

    A way to fix more than once click
    It is not the best but this is my way.

--- src/muqsit/bedrockfix/EventListener.php
+++ src/muqsit/bedrockfix/EventListener.php
@@ -10,11 +10,39 @@ use pocketmine\network\mcpe\protocol\SetEntityDataPacket;
 
 class EventListener implements Listener {
 
+        protected $PlayerData;
+    
+        /** @var array $taps */
+        public $taps = [];
+    
     public function onDataPacketReceive(DataPacketReceiveEvent $event) : void
     {
         $packet = $event->getPacket();
+        if ($packet instanceof LoginPacket) {
+             $this->PlayerData[$packet->username] = $packet->clientData;
+        }
         if ($packet instanceof SetEntityDataPacket) { //thanks to @Driesboy for this
             $event->getPlayer()->respawnToAll();
         }
     }
+    
+    public function onInteract(PlayerInteractEvent $event) : void
+    {
+    $player = $event->getPlayer();
+     $os = ["Unknown", "Android", "iOS", "macOS", "FireOS", "GearVR", "HoloLens", "Windows 10", "Windows", "Dedicated", "Orbis", "NX"];
+     $cdata = $this->PlayerData[$player->getName()];
+     if ($os[$cdata["DeviceOS"]] == "Windows" or $os[$cdata["DeviceOS"]] == "Windows 10") {
+      if ($event->getAction() == $event::RIGHT_CLICK_BLOCK or $event->getAction() == $event::RIGHT_CLICK_BLOCK) {
+         $this->taps[$player->getName()]++;
+         if ($this->taps[$player->getName()] == 7) {
+         $this->taps[$player->getName()] == 0;           
+         }
+     }else{
+         $this->taps[$player->getName()] = 0;
+      }else{
+         $this->taps[$player->getName()] = 0;
+      }
+     }
+    }
+    
 }
